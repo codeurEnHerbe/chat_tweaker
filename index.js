d32bc80cdb587ec09648e0025417764dfaaac65b
Rafael <rafi01081999@gmail.com>
2023-06-19 19:35:46 +0200

    add rates to UI

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let slider_messages = document.getElementById('slider-messages');
 let slider_blocks = document.getElementById('slider-blocks');
 let buffer_count = document.getElementById('buffer-count');
 
+let block_rate = document.getElementById('block-rate');
+let message_rate = document.getElementById('message-rate');
+
 let maxInt = 1000
 let block_speed = 500
 let messageBuffer = []
@@ -13,13 +16,14 @@ let messageBuffer = []
 
 slider_messages.addEventListener('input', function () {
     maxInt = (10 - slider_messages.value) * 100
-
+    message_rate.innerHTML = maxInt == 0 ? "< 100 ms." : maxInt + " ms."
 }, false);
 
 
 slider_blocks.addEventListener('input', function () {
     block_speed = map(slider_blocks.value, [0, 10], [50, 800])
     console.log(block_speed)
+    block_rate.innerHTML = block_speed == 0 ? "< 100 ms." : block_speed + " ms."
 }, false);
 
 
@@ -64,7 +68,6 @@ function flushArray() {
     })
 
     messageBuffer = []
-    console.log(block_speed)
     setTimeout(flushArray, block_speed);
 }
 
