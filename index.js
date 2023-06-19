64fd1c0300b6bfbaaf309c71bbcad5551c548464
Rafael <rafi01081999@gmail.com>
2023-06-19 14:49:34 +0200

    Add block speed + some messages

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
+
+import data from './messages.json' assert { type: 'json' };
+
 let feed = document.getElementById('chatfeed')
-let slider = document.getElementById('slider');
-import data from './messages.json' assert { type: 'json' };;
+let slider_messages = document.getElementById('slider-messages');
+let slider_blocks = document.getElementById('slider-blocks');
+let buffer_count = document.getElementById('buffer-count');
+
 let maxInt = 1000
+let block_speed = 500
+let messageBuffer = []
+
+
+slider_messages.addEventListener('input', function () {
+    maxInt = (10 - slider_messages.value) * 100
+
+}, false);
+
+
+slider_blocks.addEventListener('input', function () {
+    block_speed = map(slider_blocks.value, [0, 10], [50, 800])
+    console.log(block_speed)
+}, false);
+
 
 function map(value, oldRange, newRange) {
     var newValue = (value - oldRange[0]) * (newRange[1] - newRange[0]) / (oldRange[1] - oldRange[0]) + newRange[0];
     return Math.min(Math.max(newValue, newRange[0]), newRange[1]);
 }
 
-slider.addEventListener('input', function () {
-    maxInt = (11 - slider.value) * 100
-
-}, false);
 
 function updateScroll() {
     feed.scrollTo(0, feed.scrollHeight)
 }
 
-function createNewRandomMessage() {
+function buildNewMessage(message) {
     if (feed.children.length > 30) {
         feed.removeChild(feed.firstElementChild)
     }
+    message.replace('\"', '"')
 
-    var randomMessage = data.messages[Math.floor(Math.random() * data.messages.length)];
-    randomMessage.replace('\"', '"')
-    feed.innerHTML += randomMessage
+    feed.innerHTML += message
+    buffer_count.innerHTML = messageBuffer.length
 }
 
 
@@ -35,14 +51,30 @@ function main() {
     if (iteration > 500) {
         return;
     }
-    createNewRandomMessage()
+
+
+    let indexRandomMess = Math.floor(Math.random() * data.messages.length)
+    messageBuffer.push(indexRandomMess)
+
     updateScroll()
     let interval = Math.floor(Math.random() * (maxInt - 50 + 1) + 50)
     setTimeout(main, interval);
     iteration++
 }
 
+function flushArray() {
+
+    messageBuffer.forEach(messageIndex => {
+        buildNewMessage(data.messages[messageIndex])
+    })
+
+    messageBuffer = []
+    console.log(block_speed)
+    setTimeout(flushArray, block_speed);
+}
+
 main()
+flushArray()
 
 
 
